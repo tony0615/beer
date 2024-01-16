fad31963ad6ce6c07961ee36ce5a397299971c95
tony <tony_sword@163.com>
2024-01-16 09:26:36 +0800

    2024-01-16

---
+++ wwwroot/js/site.js
@@ -0,0 +1,85 @@
+﻿
+$(function () {
+    let ww = $(window).width(), hh = $(window).height();
+    //let a = $("header").height() || 0, b = $("footer").height() || 0;
+    //$(".sf-height").height(hh - a - b);
+    $(document).on("change", ".file-movie", movieupload)
+})
+function movieupload(e) {
+    e = e.target;
+    var ds = $(e).next(".file-movie-url");
+    if (!ds) return;
+   
+    var date = new Date();
+    var nm = "video/" + date.getFullYear() + '' + date.getMonth() + '' + date.getDate() + Math.floor(Math.random() * 1000000);
+    
+    const data = new FormData();
+    data.append('file', e.files[0]);
+    data.append('allowSize', 1000);
+    gwebmsg("/api/TokenQiniu?key=" + nm, function (x) {
+        data.append('token', x.token);
+        data.append('key', nm)
+        //console.log(x.token)
+        $.ajax({
+            url: "https://upload-z2.qiniup.com",
+            type: "POST",
+            data: data,
+            dataType: "JSON",
+            contentType: false,
+            processData: false,
+            success: function (r) {
+                if (r) {
+                    $(ds).val("https://image.szwushu.org/" + r.key);
+                } else {
+                    Msgbox("上传图片失败");
+                }
+            }
+        })
+    })
+}
+function webmsg(_url, _data, _rvf) {
+    $.ajax({
+        type: "post",
+        url: _url,
+        data: _data,
+        dataType: 'text',
+        success: function (rt) {
+            console.log(rt)
+            if (rt) {
+                try {
+                    rt = eval('(' + rt + ')');
+                } catch(e) {
+
+                }
+                if (_rvf) _rvf(rt);
+            }
+        },
+        complete: function (rx) {
+            rx = null;
+        }
+    });
+}
+function gwebmsg(_url, _rvf) {
+    $.ajax({
+        type: "get",
+        url: _url,
+        dataType: 'text',
+        success: function (rt) {
+            console.log(rt)
+            if (rt) {
+                try {
+                    rt = eval('(' + rt + ')');
+                } catch (e) {
+
+                }
+                if (_rvf) _rvf(rt);
+            }
+        },
+        complete: function (rx) {
+            rx = null;
+        }
+    });
+}
+function Msgbox(i) {
+    alert(i);
+}
---
+++ wwwroot/lib/Editor/ckeditor_uploader.js
@@ -0,0 +1,101 @@
+﻿class UploadAdapter {
+    constructor(loader) {
+        this.loader = loader;
+    }
+    upload() {
+        return new Promise((resolve, reject) => {
+            if (Config_UploadImage == "local") {
+                try {
+                    let file = this.loader.file;
+                    //let URL = window.URL || window.webkitURL;
+                    //let blob = URL.createObjectURL(file);
+                    
+                    if (window.FileReader) {
+                        let reader = new FileReader();
+                        //resolve({ default: reader.readAsDataURL(file) });
+
+                        reader.onload = function (event) {
+                            let base64;
+                            let img = new Image();
+                            img.src = event.target.result;
+
+                            img.onload = function () {
+
+                                let that = this;
+                                let w = that.width, h = that.height, scale = w / h;
+                                w = Config_UploadImage_width || w;
+                                h = w / scale;
+
+                                let canvas = document.createElement('canvas');
+                                let ctx = canvas.getContext('2d');
+                                $(canvas).attr({
+                                    width: w,
+                                    height: h
+                                });
+                                ctx.drawImage(that, 0, 0, w, h);
+                                base64 = canvas.toDataURL('image/jpeg', Config_UploadImage_quality || 0.8);
+                                console.log("Image:" + base64);
+                                resolve({ default: base64 });
+                            }
+                        }
+                        try {
+                            reader.readAsDataURL(file);
+                        } catch (e) {}
+                    } else {
+                        let date = new Date();
+
+                        let nm = date.getFullYear() + '' + date.getMonth() + '' + date.getDate() + Math.floor(Math.random() * 1000000);
+
+                        let data = new FormData();
+                        data.append('file', this.loader.file);
+                        data.append('asize', 10);
+                        $.ajax({
+                            url: "/api/UploadImage?sfz=" + nm,
+                            type: "POST",
+                            data: data,
+                            dataType: "JSON",
+                            contentType: false,
+                            processData: false,
+                            success: function (r) {
+                                console.log(r);
+                                if (r) {
+                                    resolve({ default: "/imgs/" + r.key });
+                                } else {
+                                    reject("上传图片失败");
+                                }
+                            }
+                        })
+                    }
+                } catch (e) {
+                    //console.log(e);
+                }
+            } else {
+                var date = new Date();
+
+                var nm = date.getFullYear() + '' + date.getMonth() + '' + date.getDate() + Math.floor(Math.random() * 1000000);
+
+                const data = new FormData();
+                data.append('file', this.loader.file);
+                data.append('allowSize', 10);
+                gwebmsg("/api/TokenQiniu?Key=" + nm, function (x) {
+                    data.append('token', x);
+                    $.ajax({
+                        url: "https://upload-z2.qiniup.com",
+                        type: "POST",
+                        data: data,
+                        dataType: "JSON",
+                        contentType: false,
+                        processData: false,
+                        success: function (r) {
+                            if (r) {
+                                resolve({ default: "https://image.szwushu.org/"+r.key });
+                            } else {
+                                reject("上传图片失败");
+                            }
+                        }
+                    })
+                })
+            }
+        });
+    }
+}
